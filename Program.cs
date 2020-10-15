07f0936bb66247f898f91bbed9da7732612dd408
Valerian21 <72431080+Valerian21@users.noreply.github.com>
2020-10-15 13:56:49 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InspectorLib;
+
+namespace PlyaskinaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Запускаются методы из библиотеки 
+                FunctionInsp.GetInspector();             
+               FunctionInsp.GetСarInspection();
+                //Т.к метод не стаcтический, 
+                //его можно вызвать только через экземпляр класса.
+                var GWor = new FunctionInsp();
+                GWor.GetWorker();
+                var Insp = new FunctionInsp();
+               Insp.SetInspector("");
+               FunctionInsp.GenerateNumber(0,"",0);
+                var AWor = new FunctionInsp();
+               AWor.AddWorker();
+               Console.ReadLine();
+            }
+
+        }
+    }
+}
